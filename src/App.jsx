9a654dc664c4c01b554dbc979d1d49b8157f2ec7
abdul-korim-web <moneyschorsing@gmail.com>
2025-10-13 14:34:16 +0600

    loaddig Added:

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
-
-import './App.css'
-import {HeroUIProvider} from "@heroui/react";
-import NavArea from "./Components/Navbar/navArea"
+import "./App.css";
+import { HeroUIProvider } from "@heroui/react";
+import NavArea from "./Components/Navbar/navArea";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import WeatherHero from './Components/WeatherHero/WeatherHero';
-import Contact from './Components/Contact/Contact';
-import About from './Components/About/About';
+import WeatherHero from "./Components/WeatherHero/WeatherHero";
+import Contact from "./Components/Contact/Contact";
+import About from "./Components/About/About";
+import { useEffect, useState } from "react";
+import { SyncLoader } from "react-spinners";
+
 
 function App() {
- 
+  const [isLoading, setisLoading] = useState(true);
+  useEffect(() => {
+    const Timer = setTimeout(() => {
+      setisLoading(false);
+    }, 1500);
 
+    return () => clearTimeout(Timer);
+  }, []);
+  if(isLoading){
+    return(
+      <div className="bg-black flex flex-col min-h-screen justify-center items-center ">
+        <SyncLoader color="white" />
+      </div>
+    )
+  }
 
   return (
     <BrowserRouter>
-    <HeroUIProvider>
-      <NavArea/>
-      <Routes>
-        <Route path='/' element={<WeatherHero/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-        <Route path='/about' element={<About/>}/>
-      </Routes>
-
-     
-    </HeroUIProvider>
-
-
+      <HeroUIProvider>
+        <NavArea />
+        <Routes>
+          <Route path="/" element={<WeatherHero />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </HeroUIProvider>
     </BrowserRouter>
-    
-  )
+  );
 }
 
-export default App
+export default App;
